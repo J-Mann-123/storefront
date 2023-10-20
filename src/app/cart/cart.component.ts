564343a5e1b1d06b7ea2d02894c1c1978481038e
J-Mann-123 <jmann5326@gmail.com>
2023-10-20 16:03:57 +1030

    Added functionality to add products to cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductItemDetailService } from '../services/product-item-detail.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  products: any[] = [];
+  constructor(private productItemDetailService: ProductItemDetailService) { }
+
+  ngOnInit () {
+    this.products = this.productItemDetailService.getproduct()
+    console.log('cart products', this.products)
+  }
 
 }
